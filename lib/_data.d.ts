85a46bef5b3156547cedc9290fe51df860cfacd8
Kennith Grotjohn <kwgrotjohn@gmail.com>
2024-09-26 12:37:12 -0700

    Partial - Convert to direct data pull

--- lib/_data.d.ts
+++ lib/_data.d.ts
@@ -1,3 +1,152 @@
+export interface RawArchetype extends RawData<Archetype> {}
+export interface RawRank extends RawData<Rank> {}
+export interface RawTalent extends RawData<Talent> {}
+export interface RawTree extends RawData<Tree> {}
+
+declare namespace loc {
+  type table = string;
+  type row = string;
+  type fallback = string;
+  type func = `NSLOCTEXT(\\"${table}\\", \\"${row}\\", \\"${fallback}\\")"`;
+}
+
+type pngPath = `/Game/Assets/2DArt/${string}`;
+
+interface RawData<T extends basicTemplate> {
+  /** Not needed for our purposes */
+  RowStruct: string;
+  /** A set of defaults that can be filled in per row */
+  Defaults: T;
+  /** Not needed for our purposes */
+  Columns?: { any: any }[];
+  Rows: [T];
+}
+
+type dlcName = "Laika" | "NewFrontiers" | "Styx" | "Galileo";
+interface BasicTemplate {
+  /**
+   * This is the internal identifier
+   */
+  Name: string;
+  /**
+    This will be a localization function call written as a string
+
+    The result of the call will be the user-viewable name.
+   */
+  DisplayName?: loc.func;
+  /**
+   * Fully qualified path to the icon from the game root, E.G. ```"/Game/Assets/2DArt/UI/Icons/Icon_TalentRank0.Icon_TalentRank0"```
+   */
+  Icon?: pngPath;
+  /**
+   * This can appear in any element to indicate that a named patch is required for it
+   *
+   * I don't think this matters for our purposes.
+   * */
+  Metadata?: {
+    RequiredFeatureLevel: {
+      RowName: dlcName;
+    };
+  };
+}
+
+interface Rank extends BasicTemplate {
+  DisplayName;
+  Icon;
+  /**
+   * How many total points must be invested in a tree to reach this rank.
+   */
+  Investment?: number;
+  /**
+   * The internal id of the rank after this one.
+   */
+  NextRank?: {
+    /** This needs to match a name from D_TalentRanks.json */
+    RowName: string;
+  };
+}
+
+/**
+ * For player talents, this is the tab.
+ * For creature talents, this is the creature.
+ * For blueprints, this is the tier.
+ * For prospects, this is the prospect.
+ * For workshop items, this is the category.
+ */
+type archetypeModels =
+  | "Player"
+  | "Creature"
+  | "Blueprint"
+  | "Prospect"
+  | "Workshop";
+interface Archetype extends BasicTemplate {
+  DisplayName;
+  Icon;
+  /**
+   * This tells us what category this archetype falls under.
+   */
+  Model: {
+    RowName: archetypeModels;
+  };
+  RequiredLevel?: number;
+}
+
+type BlankBackground = "/Game/Assets/2DArt/UI/Windows/EmptyAsset.EmptyAsset";
+interface Tree extends BasicTemplate {
+  DisplayName;
+  /**
+   * Fully qualified path to the background from the game root, E.G. ```"/Game/Assets/2DArt/UI/Talents/Backgrounds/Adventure_Fishing_Background.Adventure_Fishing_Background"```
+   */
+  BackgroundTexture?: pngPath;
+  /** This needs to match a name from D_TalentArchetypes.json */
+  Archetype: {
+    RowName: string;
+  };
+  FirstRank?: {
+    RowName: string;
+  };
+  RequiredLevel?: number;
+}
+
+/**
+ * This massively overloaded type is not just player and creature talents, but also blueprints, workshop items, and prospects.
+ */
+type Talent = PlayerTalent;
+interface PlayerTalent extends BasicTemplate {
+  /**
+   * Reroutes are non-intractable, invisible nodes used to form intersections in the requirement trees.
+   */
+  bIsReroute: boolean;
+  DisplayName;
+  Description;
+  Icon;
+  TalentTree: {
+    /** This needs to match a name from D_TalentTrees.json */
+    RowName: string;
+  };
+  Position: {
+    X: 0;
+    Y: 0;
+  };
+  Size: {
+    X: 64;
+    Y: 64;
+  };
+  Rewards: {
+    GrantedStats: { string: number };
+    GrantedFlags: string[];
+  }[];
+  RequiredTalents?: { RowName: string; DataTableName: "D_Talents" }[];
+  RequiredFlags?: [];
+  ForbiddenFlags?: [];
+  RequiredRank: {
+    /** This needs to match a name from D_TalentRanks.json */
+    RowName: string;
+  };
+  RequiredLevel: number;
+  bDefaultUnlocked: boolean;
+}
+
 export interface Talent {
   readonly name: string;
   readonly desc: string;
@@ -9,15 +158,7 @@ export interface Talent {
   readonly values: number[];
   readonly img_name: string;
 }
-export interface RawTalent extends Talent {
-  section: string;
-  tab: string;
-  id: string;
-}
-export interface RawData {
-  talents: RawTalent[];
-  affects: { [id: string]: string };
-}
+
 export interface Tab {
   background: string;
   talents: { [talentId: string]: Talent };
--- lib/dataParsed.ts
+++ lib/dataParsed.ts
@@ -1,35 +1,37 @@
-import rawData from "./_data";
-import { Data, State, Talent } from "./_data";
+import rawData, { Data, RawData, State, Talent } from "./_data";
+import rawTalents from "./MinedData/D_TalentArchetypes.json";
 
 export const data: Data = {};
-export const initialState: State = {};
 
-rawData.talents.forEach((talent) => {
-  const { section, tab, id } = talent;
-  let parsedTalent: Talent = {
-    name: talent.name,
-    desc: talent.desc,
-    reqs: talent.reqs,
-    y: talent.y,
-    x: talent.x,
-    tier: talent.tier,
-    affect: rawData.affects[talent.affect],
-    values: talent.values,
-    img_name: talent.img_name,
-  };
+// export const initialState: State = {};
 
-  // Populate the objects with the reformatted data
-  data[section] ??= {
-    background: "",
-    tabs: {},
-  };
-  data[section].tabs[tab] ??= {
-    background: "",
-    talents: {},
-  };
-  data[section].tabs[tab].talents[id] = parsedTalent;
+// rawData.talents.forEach((talent) => {
+//   const { section, tab, id } = talent;
+//   let parsedTalent: Talent = {
+//     name: talent.name,
+//     desc: talent.desc,
+//     reqs: talent.reqs,
+//     y: talent.y,
+//     x: talent.x,
+//     tier: talent.tier,
+//     affect: rawData.affects[talent.affect],
+//     values: talent.values,
+//     img_name: talent.img_name,
+//   };
 
-  initialState[section] ??= {};
-  initialState[section][tab] ??= {};
-  initialState[section][tab][id] = { rank: 0, max: talent.values.length };
-}); // END rawData.talents.forEach
+//   // Populate the objects with the reformatted data
+//   data[section] ??= {
+//     background: "",
+//     tabs: {},
+//   };
+//   data[section].tabs[tab] ??= {
+//     background: "",
+//     talents: {},
+//   };
+//   data[section].tabs[tab].talents[id] = parsedTalent;
+
+//   initialState[section] ??= {};
+//   initialState[section][tab] ??= {};
+//   initialState[section][tab][id] = { rank: 0, max: talent.values.length };
+// }
+// ); a// END rawData.talents.forEach
